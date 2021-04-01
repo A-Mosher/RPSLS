3d63103167327405f5f13981b52d002efee5555e
A-Mosher <adam.l.mosher@gmail.com>
2021-03-31 22:38:56 -0400

    Added what beats what key at the bottom of game.js.

--- game.js
+++ game.js
@@ -67,5 +67,13 @@ class Game {
     }
     
 }
+let gestures = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
+let rockBeats = (gestures[0] > gestures[2,3]);
+let paperBeats = (gestures[1] > gestures[0,4]);
+let scissorsBeats = (gestures[2] > gestures[1,3]);
+let LizardBeats = (gestures[3] > gestures[5,1]);
+let SpockBeats = (gestures[5] > gestures[0,3]);
+
+
 
 module.exports.Game = Game;
